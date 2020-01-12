51615972fe155d8f0a82fb7d76fffa7ce2d2914c
PedroCoza <56022203+PedroCoza@users.noreply.github.com>
2020-01-12 20:44:15 +0100

    Changes to Phone pattern reverted

--- src/main/java/acme/datatypes/Phone.java
+++ src/main/java/acme/datatypes/Phone.java
@@ -6,6 +6,8 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
+import org.hibernate.validator.constraints.Range;
+
 import acme.framework.datatypes.DomainDatatype;
 import lombok.Getter;
 import lombok.Setter;
@@ -21,9 +23,8 @@ public class Phone extends DomainDatatype {
 	private static final long	serialVersionUID	= 1L;
 
 	@NotNull
-	//@Range(min = 1, max = 999)
-	@Pattern(regexp = "^([+]{1})([0]{0,}?[1-9]{1,3})$")
-	private String				countryCode;
+	@Range(min = 1, max = 999)
+	private Integer				countryCode;
 
 	@Pattern(regexp = "\\d{1,6}", message = "default.error.conversion")
 	private String				areaCode;
